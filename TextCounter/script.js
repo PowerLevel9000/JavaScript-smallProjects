c92d1d39ef9e7c768085cfc3e5d278c3e21a263d
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-01-07 12:08:11 +0530

    debuged TextCounter

--- TextCounter/script.js
+++ TextCounter/script.js
@@ -7,12 +7,12 @@ const symbols = document.getElementById('symbols');
 function count() {
   if (text.value.length === 0) {
     stats.style.display = 'none';
-  }else {
+  } else {
     stats.style.display = 'block';
-    char.innerHTML = text.value.length + ' Characters';// char
-    words.innerHTML = text.value.trim().split(/\s+/).length + ' Words'; // words
-    lines.innerHTML = text.value.split('\n').length + ' Lines'; // lines
-    symbols.innerHTML = text.value.split(/[!@#$%^&*+()_={};:'"<>.,?/-]/).length + ' Symbols';
+    char.innerHTML = `${text.value.length} Characters`;// char
+    words.innerHTML = `${text.value.trim().split(/\s+/).length} Words`; // words
+    lines.innerHTML = `${text.value.split('\n').length} Lines`; // lines
+    symbols.innerHTML = `${text.value.split(/[!@#$%^&*+()_={};:'"<>.,?/-]/).length - 1} Symbols`;
   }
 }
 text.addEventListener('input', count);
