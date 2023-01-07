81f552c8f15a34526e9b256bd38c47bbbc2ccba9
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-01-07 11:54:19 +0530

    debugging initial

--- Stopwatch/script.js
+++ Stopwatch/script.js
@@ -1,38 +1,36 @@
-window.onload = function () {
-  let seconds = 0; 
-  let tens = 0; 
-  let appendTens = document.getElementById("tens"); 
-  let appendSeconds = document.getElementById("seconds"); 
-  let buttonStart = document.getElementById("button-start"); 
-  let buttonStop = document.getElementById("button-stop"); 
-  let buttonReset = document.getElementById("button-reset"); 
+window.onload = () => {
+  let seconds = 0;
+  let tens = 0;
+  const appendTens = document.getElementById('tens');
+  const appendSeconds = document.getElementById('seconds');
+  const buttonStart = document.getElementById('button-start');
+  const buttonStop = document.getElementById('button-stop');
+  const buttonReset = document.getElementById('button-reset');
   let Interval;
 
-  buttonStart.onclick = function () {
+  buttonStop.onclick = () => { clearInterval(Interval); };
+  buttonReset.onclick = () => {
     clearInterval(Interval);
-    Interval = setInterval(startTimer, 10);
-  };
-
-  buttonStop.onclick = function () { clearInterval(Interval); };
-  buttonReset.onclick = function () {
-    clearInterval(Interval);
-    tens = "00";
-    seconds = "00";
+    tens = '00';
+    seconds = '00';
     appendTens.innerHTML = tens;
     appendSeconds.innerHTML = seconds;
   };
 
   function startTimer() {
-    tens++;
-    if (tens <= 9) { appendTens.innerHTML = "0" + tens; }
+    tens += 1;
+    if (tens <= 9) { appendTens.innerHTML = `0${tens}`; }
     if (tens > 9) { appendTens.innerHTML = tens; }
     if (tens > 99) {
-      console.log("seconds");
-      seconds++;
-      appendSeconds.innerHTML = "0" + seconds;
+      seconds += 1;
+      appendSeconds.innerHTML = `0${seconds}`;
       tens = 0;
-      appendTens.innerHTML = "0" + 0;
+      appendTens.innerHTML = `0${0}`;
     }
     if (seconds > 9) { appendSeconds.innerHTML = seconds; }
   }
+  buttonStart.onclick = () => {
+    clearInterval(Interval);
+    Interval = setInterval(startTimer, 10);
+  };
 };
